d3f21a1fa75f3a0ffedfdfe3d7b75bfe80eed6cf
Martin Duvanel <martin.duvanel@a3.epfl.ch>
2019-08-11 16:40:07 +0200

    Switch old man to hero

--- app/main.rb
+++ app/main.rb
@@ -4,7 +4,7 @@ $dragon.require 'app/scene.rb'
 
 $scene = Scene.new(1000, 500)
 $scene.add_background_element(WoodenFloor.new([200, 160], 28, 16))
-$scene.add_character(Character.new('sprites/old_man.png', 640, 360))
+$scene.add_character(Character.new('sprites/hero.png', 640, 360))
 
 def tick args
   $scene.tick(args.inputs, args.outputs)
